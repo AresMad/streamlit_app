1c8434c48d0d8f29e58569679c999e3dda594cd8
AresMad <155627570+AresMad@users.noreply.github.com>
2024-01-04 21:04:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ except URLError as e:
 
 def get_fuit_load_list():
   with my_cnx.cursor() as my_cur:
-    my_cnx.execute("select * from fuit_load_list")
+    my_cnx.execute("select * from fruit_load_list")
     return my_cnx.fetchall()
     
 if streamlit.button('Get Fruit LOAD List'):
