8b0a5d23bf8de1ce70e453c9361fb092ed78944e
AresMad <155627570+AresMad@users.noreply.github.com>
2024-01-04 17:18:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit
 streamlit.title('Application Title')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('food 1')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
